35841308d985ff3478d56af6e9beb71384940aef
afriz <afrizaputra005@gmail.com>
2025-09-19 11:06:31 +0700

    initial commit

---
+++ src/CharAnggo.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+    var CharA : Char = 'A'
+    println(CharA.isUpperCase())
+    println(CharA.isLowerCase())
+    println(CharA.isDigit())
+    println(CharA.toLowerCase())
+    val strA: String = CharA. toString()
+    println("Kini charA sudah menjadi String = "+strA)
+
+}
---
+++ src/String.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>){
+    val myName: String = "Afriza Putra Pastinya"
+    for (chr in myName){
+        print(chr)
+    }
+    print('\n')
+}
